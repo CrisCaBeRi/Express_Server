f72e81048bb742069a444d09ecea754da0a4cfbd
Cristian Camilo Betancourt Rincon <cristian.camilo.betancourt.rin@gmail.com>
2023-01-25 11:22:44 -0500

    Final steps to create a express server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hola mundo")
+const express = require("express"); // se vincula una variable a la dependenciua
+const app = express();//Vinculación de la lectura por medio de express
+const port = 3000; //Definición del puerto
+
+
+//req son los elementos que se toman del usuario y res es la respuesta que se le da al ususario
+// metodo .get el slach es para indicar que es la raiz
+app.get("/", (req,res)=>{
+  res.send("hola mundo"); // envia ino a pantalla
+})
+
+//Se vincula la app con el puerto mediante el metodo .listen
+app.listen(port, ()=>{
+  console.log("mi puerto es " + port)
+})
+
+
+
+
